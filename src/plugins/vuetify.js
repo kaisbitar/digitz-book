671b304c05841c8e6ba612be85b89c34be646f03
kaisbitar <kais.bitar@yahoo.com>
2020-04-22 00:18:23 -0600

    adding inital project with project map file

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import ar from "vuetify/es5/locale/ar";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  rtl: true,
+  lang: {
+    locales: { ar },
+    current: "ar"
+  }
+});
