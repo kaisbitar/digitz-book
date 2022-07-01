2cf224ba6e5595c3b4415075fad12dcdc8d58e5b
Kais <kais@Halas-MacBook-Pro.local>
2022-07-01 02:09:26 +0100

    Added linux deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 npm run build
 
 
-tar.exe cvzf build.tar dist
+#// windows tar.exe cvzf build.tar dist
+tar cvzf build.tar dist
 scp build.tar root@167.99.39.94:/mysites/digitz-fe
 
 ssh root@167.99.39.94 -C 'cd /mysites/digitz-fe && tar -zxvf build.tar && rm -rf app-prev && mv app app-prev && mv dist app && rm build.tar'
