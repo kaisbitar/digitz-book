bd22ae52c55577c320397f13d145041e7037f5ba
Kais Bitar <k.bitar@sensory-minds.com>
2024-09-29 18:34:24 +0100

    added closing button

--- src/components/VerseDetails.vue
+++ src/components/VerseDetails.vue
@@ -13,28 +13,7 @@
         </v-btn>
       </v-toolbar>
       <v-container>
-        <div class="text-h3">
-          <span v-for="(word, index) in verseWords" :key="index">
-            <v-tooltip bottom>
-              <template v-slot:activator="{ props }">
-                <span
-                  v-bind="props"
-                  class="mx-1"
-                  @mouseover="displayMeaning(word)"
-                  >{{ word }}</span
-                >
-              </template>
-              <span v-if="meanings[word]">
-                <span
-                  v-for="(meaning, i) in meanings[word]"
-                  :key="i"
-                  v-html="meaning"
-                ></span>
-              </span>
-              <span v-else>Loading...</span>
-            </v-tooltip>
-          </span>
-        </div>
+        <VerseToolTip :verse="verse" />
         <AppCountChips
           :wordCount="countVerseWords(verse)"
           :letterCount="countVerseLetters(verse)"
@@ -47,26 +26,14 @@
 
 <script setup>
 import { useCounting } from "@/mixins/counting"
-import { fetchWordMeaning } from "../api/api.js"
 
-const appApi = import.meta.env.VITE_APP_API_URL
 const emit = defineEmits(["update:modelValue"])
 const { countVerseWords, countVerseLetters } = useCounting()
-const meanings = reactive({})
 
 const props = defineProps({
   modelValue: Boolean,
   verse: String,
 })
-
-const verseWords = computed(() => props.verse.split(" "))
-
-const displayMeaning = async (word) => {
-  if (!meanings[word]) {
-    // const response = await fetchWordMeaning(appApi, word)
-    // meanings[word] = response
-  }
-}
 
 const handleClose = () => {
   emit("update:modelValue", false)
