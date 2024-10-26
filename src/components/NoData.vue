34ce7570c563f0c9dff31f74d742002e8b00e6e8
Kais Bitar <k.bitar@sensory-minds.com>
2024-10-26 19:47:23 +0100

    added infinte scrll to tarteel

---
+++ src/components/NoData.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="6" lg="4" class="text-center">
+        <v-icon
+          icon="mdi-book-open-page-variant"
+          size="x-large"
+          class="mb-4"
+        ></v-icon>
+        <v-card elevation="0" variant="plain" class="pa-4">
+          <v-card-text class="text-h6"> ورتل القرآن ترتيلا.. </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
