ae0427b0760543efd2b04db3b185ec8ae1a84e7f
Kais Bitar <k.bitar@sensory-minds.com>
2025-04-07 20:34:20 +0100

    Made the wordFilter work for word and words root

--- src/utils/wordFilter/results.ts
+++ src/utils/wordFilter/results.ts
@@ -2,22 +2,25 @@ import { Results, SortedResultItem, FilterResult } from './types'
 
 export const sortResults = (
   results: Results,
-  searchTerm: string
+  searchTerm: string,
+  root?: string
 ): SortedResultItem[] => {
     return Object.entries(results)
     .map(([word, { count, score, verses }]) => ({ word, count, score, verses }))
     .sort((a, b) => {
-      // Prioritize exact matches
-      if (a.word === searchTerm && b.word !== searchTerm) return -1
-      if (b.word === searchTerm && a.word !== searchTerm) return 1
+      // Prioritize exact matches with either searchTerm or root
+      const aIsExactMatch = a.word === searchTerm || a.word === root
+      const bIsExactMatch = b.word === searchTerm || b.word === root
+      if (aIsExactMatch && !bIsExactMatch) return -1
+      if (bIsExactMatch && !aIsExactMatch) return 1
 
-      // Prioritize words starting with the search term
-      const aStartsWith = a.word.startsWith(searchTerm)
-      const bStartsWith = b.word.startsWith(searchTerm)
+      // Prioritize words starting with either searchTerm or root
+      const aStartsWith = a.word.startsWith(searchTerm) || (root ? a.word.startsWith(root) : false)
+      const bStartsWith = b.word.startsWith(searchTerm) || (root ? b.word.startsWith(root) : false)
       if (aStartsWith && !bStartsWith) return -1
       if (bStartsWith && !aStartsWith) return 1
 
-      // For words that both start with the search term, sort by length
+      // For words that both start with searchTerm or root, sort by length
       if (aStartsWith && bStartsWith) {
         return a.word.length - b.word.length
       }
